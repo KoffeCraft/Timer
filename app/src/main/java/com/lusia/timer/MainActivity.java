dd2e4aec686acc506c577bdec1511bcdbda94a03
KoffeCraft <lucycraft.minecrafte@gmail.com>
2026-03-31 08:10:28 +0200

    Button i wywietlanie czasu

--- app/src/main/java/com/lusia/timer/MainActivity.java
+++ app/src/main/java/com/lusia/timer/MainActivity.java
@@ -2,6 +2,8 @@ package com.lusia.timer;
 
 import android.os.Bundle;
 import android.os.Handler;
+import android.view.View;
+import android.widget.Button;
 import android.widget.TextView;
 
 import androidx.activity.EdgeToEdge;
@@ -13,6 +15,8 @@ import androidx.core.view.WindowInsetsCompat;
 public class MainActivity extends AppCompatActivity {
     private int sekundy = 0;
     private TextView textView;
+    private boolean czyDziala = false;
+    private Button buttonStart, buttonStop, buttonReset, buttonZapisz;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,14 +29,45 @@ public class MainActivity extends AppCompatActivity {
             return insets;
         });
         textView = findViewById(R.id.TextViewCzas);
+        buttonStart = findViewById(R.id.buttonStart);
+        buttonStop = findViewById(R.id.buttonStop);
+        buttonReset = findViewById(R.id.buttonReset);
+        buttonZapisz = findViewById(R.id.buttonZapisz);
         Handler handler = new Handler();
         handler.post(new Runnable() {
             @Override
             public void run() {
-                sekundy++;
-                textView.setText(String.valueOf(sekundy));
+                if(czyDziala) {
+                    sekundy++;
+                    textView.setText(wyswietlCzas(sekundy));
+                }
                 handler.postDelayed(this, 1000);
             }
         });
+        buttonStart.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                czyDziala = true;
+            }
+        });
+        buttonStop.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                czyDziala = false;
+            }
+        });
+        buttonReset.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                sekundy = 0;
+            }
+        });
+
+    }
+    private String wyswietlCzas(int ile){
+        int sekundy = ile%60;
+        int minuty = (ile/60)%60;
+        int godziny = ile/3600;
+        return String.format("%02d:%02d:%02d", godziny, minuty, sekundy);
     }
 }
